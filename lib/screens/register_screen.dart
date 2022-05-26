39044f0a4100d7c80c2ddc51234062cc9409beeb
Xineohp <vergelesalina@gmail.com>
2022-05-26 23:17:06 +0200

    first commit

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Text('hello'),),
+    );
+  }
+}
