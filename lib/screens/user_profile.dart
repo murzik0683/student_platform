5d4e23cf0872b9a8f1296b120b966957c7712d6e
Anastasiya <murzik06.83@mail.ru>
2022-06-14 23:17:49 +0300

    commit_nastya

---
+++ lib/screens/user_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget {
+  const UserProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Личный профиль'),
+      ),
+    );
+  }
+}
